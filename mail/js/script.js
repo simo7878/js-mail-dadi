97e0a2b7c1df0ab2e0753872be7d54eadf468e35
simona <palumbosimona118@gmail.com>
2020-01-08 08:23:32 +0100

    riconoscimento email

--- mail/js/script.js
+++ mail/js/script.js
@@ -4,6 +4,19 @@ alert('Benvenuto!');
 var listaEmail = ['ladyes77@hotmail.it', 'simosimo7878@libero.it', 'francescadecesare@yahoo.it', 'nickspan@gmail.com'];
 
 //chiedere email all'utente
-var emailUtente = (prompt('inserisci la tua email per verificare se sei tra gli utenti'));
+var emailUtente = prompt('inserisci la tua email');
 console.log(emailUtente);
-var emailPresente = false;
+var mailPresente = false;
+
+//verifica presenza email nella listaEmails
+for (var i = 0; i < listaEmail.length; i++) {
+  var lista = listaEmail[i];
+  if(lista == mailPresente){
+    mailPresente = true;
+  }
+}
+if (mailPresente == true){
+  alert('Benvenuto nella tua area riservata');
+}else{
+  alert('Email non riconosciuta');
+}
