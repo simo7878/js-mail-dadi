b8dfc6cf8edcb79333e632132598da9a96ba1c60
simona <palumbosimona118@gmail.com>
2020-01-08 07:30:59 +0100

    player 1

---
+++ dadi/js/script.js
@@ -0,0 +1,9 @@
+alert('Start Game!');
+
+//lista numeri da 1 a 6
+var numeriDado = [1,2,3,4,5,6];
+
+// player 1 lancio dado random
+alert('turno Player 1');
+var numeroPlayer1 = Math.floor(Math.random() * 6) + 1;
+console.log(numeroPlayer1);
