9fbc68f7e5bda664619a1b7b807d9ee968088a8d
simona <palumbosimona118@gmail.com>
2020-01-08 08:03:59 +0100

    ex email

---
+++ mail/js/script.js
@@ -0,0 +1,9 @@
+alert('Benvenuto!');
+
+//creare lista di email
+var listaEmail = ['ladyes77@hotmail.it', 'simosimo7878@libero.it', 'francescadecesare@yahoo.it', 'nickspan@gmail.com'];
+
+//chiedere email all'utente
+var emailUtente = (prompt('inserisci la tua email per verificare se sei tra gli utenti'));
+console.log(emailUtente);
+var emailPresente = false;
